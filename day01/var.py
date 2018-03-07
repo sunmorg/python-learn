5d89b124ad2259384094c9e30f32d688a5a34c42
zennu <zennuchan@163.com>
2018-03-07 23:07:45 +0800

    变量学习

---
+++ day01/var.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Author：sunmorg
+
+'''
+    变量名可以用下划线字母数字组合  第一个不能是数字  关键字不能为变量名
+
+    变量名可以用中文  实际使用中不使用中文和拼音
+
+    语义化 gf_of_boy或者gfOfBoy
+
+    用全部大写来表示常量 eg： PIE
+
+'''
+
+name = "sunmorg"
+
+name2 = name
+
+print("hello" , name, name2)
+
+name = "zennu chan"
+
+print("hello" , name, name2)
+
