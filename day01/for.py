93b85c1a55802017fa32e7102fb65b2ef36b3aa5
zennu <zennuchan@163.com>
2018-03-08 00:19:57 +0800

    for 循环

---
+++ day01/for.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# Author：sunmorg
+
+# age_of_sunm = 18
+#
+# for i in range(3):
+#     guess_age = int(input("guess age:"))
+#     if age_of_sunm == guess_age:
+#         print("yes you got it")
+#         break
+#     elif guess_age > age_of_sunm:
+#         print("think smaller...")
+#     else:
+#         print("think bigger...")
+# else:
+#     print("you have tried too many times...")
+
+for i in range(0,10,4):
+    print("loop...",i)
